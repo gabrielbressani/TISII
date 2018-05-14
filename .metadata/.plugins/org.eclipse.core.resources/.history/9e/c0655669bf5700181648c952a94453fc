ee7ced3af3b0b1108688f330df9319591f81046b
gabrielbressani <g.bressaniribeiro@gmail.com>
2018-05-14 19:52:13 -0300

    Creating API

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/9e/c0655669bf5700181648c952a94453fc
@@ -0,0 +1,9 @@
+package upei.webserver;
+
+public class RunServer {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
--- upei/src/upei/webserver/RunServer.java
+++ upei/src/upei/webserver/RunServer.java
@@ -2,8 +2,21 @@ package upei.webserver;
 
 public class RunServer {
 
-	public static void main(String[] args) {
-		System.out.println("hello world");
+	public static void main(String[] args) throws Exception {
+		RouteConfig routeConfig = new RouteConfig();
+		Server server = new Server(routeConfig);
+
+		try {
+			server.openConnection(9006);
+			
+			System.out.println("Tecle enter para interromper o servidor!");
+			System.in.read();
+		} catch(Exception e){ 
+			e.printStackTrace();
+		} finally {
+			server.closeConnection();
+			System.out.println("O servidor foi interrompido!");
+		}
 	}
 
 }
